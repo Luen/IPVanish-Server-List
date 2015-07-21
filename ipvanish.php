9c6345dc9908b2e5044b06121b6ac9310c5fcf7a
Luen Warneke <luenwarneke@gmail.com>
2015-07-21 18:49:58 +0200

    First commit

---
+++ ipvanish.php
@@ -0,0 +1,172 @@
+<?php 
+
+$domainlist = array(
+	"tia-c01.ipvanish.com",
+	"eze-c01.ipvanish.com",
+	"syd-a01.ipvanish.com",
+	"syd-a02.ipvanish.com",
+	"grz-c01.ipvanish.com",
+	"bru-b01.ipvanish.com",
+	"gru-a01.ipvanish.com",
+	"gru-a02.ipvanish.com",
+	"sof-c01.ipvanish.com",
+	"tor-c01.ipvanish.com",
+	"tor-c02.ipvanish.com",
+	"tor-c03.ipvanish.com",
+	"yvr-c01.ipvanish.com",
+	"sjo-c01.ipvanish.com",
+	"zag-c01.ipvanish.com",
+	"prg-c01.ipvanish.com",
+	"cph-c01.ipvanish.com",
+	"cph-b01.ipvanish.com",
+	"cai-c01.ipvanish.com",
+	"tll-c01.ipvanish.com",
+	"hel-c01.ipvanish.com",
+	"ory-c01.ipvanish.com",
+	"par-b01.ipvanish.com",
+	"par-a01.ipvanish.com",
+	"par-a02.ipvanish.com",
+	"drs-c01.ipvanish.com",
+	"fra-a01.ipvanish.com",
+	"fra-a02.ipvanish.com",
+	"ath-c01.ipvanish.com",
+	"hkg-c01.ipvanish.com",
+	"bud-b01.ipvanish.com",
+	"bud-c01.ipvanish.com",
+	"bud-c02.ipvanish.com",
+	"rkv-c01.ipvanish.com",
+	"bom-c01.ipvanish.com",
+	"del-c01.ipvanish.com",
+	"cgk-c01.ipvanish.com",
+	"dub-c01.ipvanish.com",
+	"lin-c01.ipvanish.com",
+	"nrt-c01.ipvanish.com",
+	"nrt-a02.ipvanish.com",
+	"nrt-a01.ipvanish.com",
+	"rix-c01.ipvanish.com",
+	"vno-c01.ipvanish.com",
+	"lux-c01.ipvanish.com",
+	"kul-c01.ipvanish.com",
+	"mex-c02.ipvanish.com",
+	"kiv-c01.ipvanish.com",
+	"ams-a01.ipvanish.com",
+	"ams-a02.ipvanish.com",
+	"ams-c01.ipvanish.com",
+	"akl-c01.ipvanish.com",
+	"osl-c01.ipvanish.com",
+	"osl-c02.ipvanish.com",
+	"pac-c01.ipvanish.com",
+	"waw-b01.ipvanish.com",
+	"waw-c01.ipvanish.com",
+	"lis-c01.ipvanish.com",
+	"otp-c01.ipvanish.com",
+	"dme-c01.ipvanish.com",
+	"ruh-c01.ipvanish.com",
+	"beg-c01.ipvanish.com",
+	"sin-c01.ipvanish.com",
+	"sin-a01.ipvanish.com",
+	"sin-a02.ipvanish.com",
+	"bts-c01.ipvanish.com",
+	"jnb-c01.ipvanish.com",
+	"sel-a01.ipvanish.com",
+	"sel-a02.ipvanish.com",
+	"mad-a01.ipvanish.com",
+	"mad-a02.ipvanish.com",
+	"mad-c01.ipvanish.com",
+	"sto-a01.ipvanish.com",
+	"sto-a02.ipvanish.com",
+	"sto-b01.ipvanish.com",
+	"zur-b01.ipvanish.com",
+	"zur-c01.ipvanish.com",
+	"bkk-c01.ipvanish.com",
+	"ist-c01.ipvanish.com",
+	"lon-a01.ipvanish.com",
+	"lon-a02.ipvanish.com",
+	"lon-c01.ipvanish.com",
+	"lon-c02.ipvanish.com",
+	"man-c01.ipvanish.com",
+	"man-c02.ipvanish.com",
+	"hrk-c01.ipvanish.com",
+	"iad-a01.ipvanish.com",
+	"iad-a02.ipvanish.com",
+	"atl-a01.ipvanish.com",
+	"atl-a02.ipvanish.com",
+	"atl-a03.ipvanish.com",
+	"chi-a01.ipvanish.com",
+	"chi-a02.ipvanish.com",
+	"chi-a03.ipvanish.com",
+	"chi-a04.ipvanish.com",
+	"dal-a01.ipvanish.com",
+	"dal-a02.ipvanish.com",
+	"las-c01.ipvanish.com",
+	"las-c02.ipvanish.com",
+	"lax-a01.ipvanish.com",
+	"lax-a02.ipvanish.com",
+	"lax-a03.ipvanish.com",
+	"mia-a01.ipvanish.com",
+	"mia-a02.ipvanish.com",
+	"mia-a03.ipvanish.com",
+	"mia-a04.ipvanish.com",
+	"nyc-a01.ipvanish.com",
+	"nyc-a02.ipvanish.com",
+	"nyc-a03.ipvanish.com",
+	"nyc-a04.ipvanish.com",
+	"phx-a01.ipvanish.com",
+	"phx-a02.ipvanish.com",
+	"phx-a03.ipvanish.com",
+	"sjc-a01.ipvanish.com",
+	"sjc-a02.ipvanish.com",
+	"sjc-a03.ipvanish.com",
+	"sjc-a04.ipvanish.com",
+	"sea-a01.ipvanish.com",
+	"sea-a02.ipvanish.com",
+	"sea-a03.ipvanish.com"
+);
+
+$iplist = array();
+foreach ($domainlist as $hostname) {
+	$hosts = gethostbynamel($hostname);
+	if (is_array($hosts)) {
+		foreach ($hosts as $ip) {
+			$iplist[] = $ip;
+		}
+	}
+}
+
+$list = "# List distributed by luenwarneke.com/ipvanish\n";
+$list .= "
+IPVanish VPN DNS:198.168.0.1-198.168.0.2
+IPVanish VPN DNS:198.18.0.1-198.18.0.2
+IPVanish VPN route-gateway:172.20.32.1-172.20.32.1
+IPVanish VPN SYSTEM ROUTING:255.255.252.255-255.255.252.255
+IPVanish VPN SYSTEM ROUTING:127.0.0.0-127.0.0.1
+IPVanish VPN SYSTEM ROUTING:255.0.0.0-255.0.0.0
+IPVanish VPN SYSTEM ROUTING:127.255.255.255-127.255.255.255
+IPVanish VPN SYSTEM ROUTING:255.255.255.255-255.255.255.255
+IPVanish VPN SYSTEM ROUTING:169.254.0.0-169.254.0.0
+IPVanish VPN SYSTEM ROUTING:169.254.56.136-169.254.56.136
+IPVanish VPN SYSTEM ROUTING:192.168.1.0-192.168.1.2
+IPVanish VPN SYSTEM ROUTING:192.168.1.255-192.168.1.255
+IPVanish VPN SYSTEM ROUTING:192.168.56.0-192.168.56.1
+IPVanish VPN SYSTEM ROUTING:192.168.56.255-192.168.56.255
+IPVanish VPN SYSTEM ROUTING:224.0.0.0-224.0.0.0
+IPVanish VPN SYSTEM ROUTING:240.0.0.0-240.0.0.0
+IPVanish VPN SYSTEM ROUTING:255.255.0.0-255.255.0.0
+IPVanish VPN SYSTEM ROUTING:172.20.35.254-172.20.35.254
+IPVanish VPN SYSTEM ROUTING:255.255.255.0-255.255.255.0
+IPVanish VPN GOOGLE DNS:8.8.8.8-8.8.8.8
+IPVanish VPN GOOGLE DNS:8.8.4.4-8.8.4.4
+IPVanish VPN network/local/netmask:172.20.32.0-172.20.32.0
+IPVanish VPN network/local/netmask:172.20.35.164-172.20.35.164
+IPVanish VPN network/local/netmask:255.255.252.0-255.255.252.0
+IPVanish VPN MASK:0.0.0.0-0.0.0.0
+IPVanish VPN MASK:128.0.0.0-128.0.0.0\n";
+
+foreach ($iplist as $ip) {
+	$list .= "IPVanish VPN:".$ip."-".$ip."\n";
+}
+
+//echo $list;
+file_put_contents('ipvanish-allowlist4.txt',$list);
+
+?>
