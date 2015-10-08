ed455aaec4955e1ef4f6a638ff2084ae1c18b1fd
Luen Warneke <luenwarneke@gmail.com>
2015-10-08 17:59:49 +0200

    Servers and script updated (Missing U.S. servers)

--- ipvanish.php
+++ ipvanish.php
@@ -1,127 +1,511 @@
 <?php 
-
-$domainlist = array(
-	"www.ipvanish.com",
-	"tia-c01.ipvanish.com",
-	"eze-c01.ipvanish.com",
-	"syd-a01.ipvanish.com",
-	"syd-a02.ipvanish.com",
-	"grz-c01.ipvanish.com",
-	"bru-b01.ipvanish.com",
-	"gru-a01.ipvanish.com",
-	"gru-a02.ipvanish.com",
-	"sof-c01.ipvanish.com",
-	"tor-c01.ipvanish.com",
-	"tor-c02.ipvanish.com",
-	"tor-c03.ipvanish.com",
-	"yvr-c01.ipvanish.com",
-	"sjo-c01.ipvanish.com",
-	"zag-c01.ipvanish.com",
-	"prg-c01.ipvanish.com",
-	"cph-c01.ipvanish.com",
-	"cph-b01.ipvanish.com",
-	"cai-c01.ipvanish.com",
-	"tll-c01.ipvanish.com",
-	"hel-c01.ipvanish.com",
-	"ory-c01.ipvanish.com",
-	"par-b01.ipvanish.com",
-	"par-a01.ipvanish.com",
-	"par-a02.ipvanish.com",
-	"drs-c01.ipvanish.com",
-	"fra-a01.ipvanish.com",
-	"fra-a02.ipvanish.com",
-	"ath-c01.ipvanish.com",
-	"hkg-c01.ipvanish.com",
-	"bud-b01.ipvanish.com",
-	"bud-c01.ipvanish.com",
-	"bud-c02.ipvanish.com",
-	"rkv-c01.ipvanish.com",
-	"bom-c01.ipvanish.com",
-	"del-c01.ipvanish.com",
-	"cgk-c01.ipvanish.com",
-	"dub-c01.ipvanish.com",
-	"lin-c01.ipvanish.com",
-	"nrt-c01.ipvanish.com",
-	"nrt-a02.ipvanish.com",
-	"nrt-a01.ipvanish.com",
-	"rix-c01.ipvanish.com",
-	"vno-c01.ipvanish.com",
-	"lux-c01.ipvanish.com",
-	"kul-c01.ipvanish.com",
-	"mex-c02.ipvanish.com",
-	"kiv-c01.ipvanish.com",
-	"ams-a01.ipvanish.com",
-	"ams-a02.ipvanish.com",
-	"ams-c01.ipvanish.com",
-	"akl-c01.ipvanish.com",
-	"osl-c01.ipvanish.com",
-	"osl-c02.ipvanish.com",
-	"pac-c01.ipvanish.com",
-	"waw-b01.ipvanish.com",
-	"waw-c01.ipvanish.com",
-	"lis-c01.ipvanish.com",
-	"otp-c01.ipvanish.com",
-	"dme-c01.ipvanish.com",
-	"ruh-c01.ipvanish.com",
-	"beg-c01.ipvanish.com",
-	"sin-c01.ipvanish.com",
-	"sin-a01.ipvanish.com",
-	"sin-a02.ipvanish.com",
-	"bts-c01.ipvanish.com",
-	"jnb-c01.ipvanish.com",
-	"sel-a01.ipvanish.com",
-	"sel-a02.ipvanish.com",
-	"mad-a01.ipvanish.com",
-	"mad-a02.ipvanish.com",
-	"mad-c01.ipvanish.com",
-	"sto-a01.ipvanish.com",
-	"sto-a02.ipvanish.com",
-	"sto-b01.ipvanish.com",
-	"zur-b01.ipvanish.com",
-	"zur-c01.ipvanish.com",
-	"bkk-c01.ipvanish.com",
-	"ist-c01.ipvanish.com",
-	"lon-a01.ipvanish.com",
-	"lon-a02.ipvanish.com",
-	"lon-c01.ipvanish.com",
-	"lon-c02.ipvanish.com",
-	"man-c01.ipvanish.com",
-	"man-c02.ipvanish.com",
-	"hrk-c01.ipvanish.com",
-	"iad-a01.ipvanish.com",
-	"iad-a02.ipvanish.com",
-	"atl-a01.ipvanish.com",
-	"atl-a02.ipvanish.com",
-	"atl-a03.ipvanish.com",
-	"chi-a01.ipvanish.com",
-	"chi-a02.ipvanish.com",
-	"chi-a03.ipvanish.com",
-	"chi-a04.ipvanish.com",
-	"dal-a01.ipvanish.com",
-	"dal-a02.ipvanish.com",
-	"las-c01.ipvanish.com",
-	"las-c02.ipvanish.com",
-	"lax-a01.ipvanish.com",
-	"lax-a02.ipvanish.com",
-	"lax-a03.ipvanish.com",
-	"mia-a01.ipvanish.com",
-	"mia-a02.ipvanish.com",
-	"mia-a03.ipvanish.com",
-	"mia-a04.ipvanish.com",
-	"nyc-a01.ipvanish.com",
-	"nyc-a02.ipvanish.com",
-	"nyc-a03.ipvanish.com",
-	"nyc-a04.ipvanish.com",
-	"phx-a01.ipvanish.com",
-	"phx-a02.ipvanish.com",
-	"phx-a03.ipvanish.com",
-	"sjc-a01.ipvanish.com",
-	"sjc-a02.ipvanish.com",
-	"sjc-a03.ipvanish.com",
-	"sjc-a04.ipvanish.com",
-	"sea-a01.ipvanish.com",
-	"sea-a02.ipvanish.com",
-	"sea-a03.ipvanish.com"
+$serverlist = array(
+	"Europe" => array(
+		"Albania" => array(
+			"Tirana" => array(
+				"tia-c01.ipvanish.com"
+			)
+		),
+		"Austria" => array(
+			"Graz" => array(
+				"grz-c01.ipvanish.com"
+			)
+		),
+		"Belarus" => array(
+			"Minsk" => array(
+				"msq-c01.ipvanish.com"
+			)
+		),
+		"Belgium" => array(
+			"Brussels" => array(
+				"bru-b01.ipvanish.com"
+			)
+		),
+		"Bulgaria" => array(
+			"Sofia" => array(
+				"sof-c01.ipvanish.com"
+			)
+		),
+		"Croatia" => array(
+			"Zagreb" => array(
+				"zag-c01.ipvanish.com"
+			)
+		),
+		"Czech Republic" => array(
+			"Holešov" => array(
+				"gtw-c01.ipvanish.com"
+			)
+		),
+		"Denmark" => array(
+			"Copenhagen" => array(
+				"cph-c01.ipvanish.com",
+				"cph-b01.ipvanish.com"
+			)
+		),
+		"Estonia" => array(
+			"Tallinn" => array(
+				"tll-c01.ipvanish.com"
+			)
+		),
+		"Finland" => array(
+			"Helsinki" => array(
+				"hel-c02.ipvanish.com"
+			)
+		),
+		"France" => array(
+			"Paris" => array(
+				"par-a01.ipvanish.com",
+				"par-a02.ipvanish.com",
+				"ory-c01.ipvanish.com"
+			)
+		),
+		"Germany" => array(
+			"Dresden" => array(
+				"drs-c01.ipvanish.com"
+			),
+			"Frankfurt" => array(
+				"fra-a01.ipvanish.com",
+				"fra-a02.ipvanish.com",
+				"fra-a03.ipvanish.com",
+				"fra-a04.ipvanish.com",
+				"fra-a05.ipvanish.com",
+				"fra-a06.ipvanish.com",
+				"fra-a07.ipvanish.com",
+				"fra-a08.ipvanish.com",
+				"fra-a09.ipvanish.com",
+				"fra-a10.ipvanish.com",
+				"fra-a11.ipvanish.com",
+				"fra-a12.ipvanish.com",
+				"fra-a13.ipvanish.com",
+				"fra-a14.ipvanish.com",
+				"fra-a15.ipvanish.com",
+				"fra-a16.ipvanish.com"
+			)
+		),
+		"Greece" => array(
+			"Athens" => array(
+				"ath-c01.ipvanish.com"
+			)
+		),
+		"Hungary" => array(
+			"Budapest" => array(
+				"bud-b01.ipvanish.com",
+				"bud-c01.ipvanish.com",
+				"bud-c02.ipvanish.com"
+			)
+		),
+		"Iceland" => array(
+			"Reykjavik" => array(
+				"rkv-c01.ipvanish.com"
+			)
+		),
+		"Ireland" => array(
+			"Dublin" => array(
+				"dub-c01.ipvanish.com",
+				"dub-c02.ipvanish.com",
+				"dub-c03.ipvanish.com"
+			)
+		),
+		"Italy" => array(
+			"Milan" => array(
+				"lin-c01.ipvanish.com",
+				"lin-c02.ipvanish.com",
+				"lin-c03.ipvanish.com",
+				"lin-c04.ipvanish.com",
+				"lin-c05.ipvanish.com",
+				"lin-c06.ipvanish.com"
+			),
+			"Rome" => array(
+				"fco-c01.ipvanish.com",
+				"fco-c02.ipvanish.com"
+			)
+		),
+		"Latvia" => array(
+			"Riga" => array(
+				"rix-c01.ipvanish.com"
+			)
+		),
+		"Lithuania" => array(
+			"Vilnius" => array(
+				"vno-c01.ipvanish.com"
+			)
+		),
+		"Luxembourg" => array(
+			"Luxembourg" => array(
+				"lux-c01.ipvanish.com"
+			)
+		),
+		"Malta" => array(
+			"Qormi" => array(
+				""
+			)
+		),
+		"Moldova" => array(
+			"Chisinau" => array(
+				"kiv-c01.ipvanish.com"
+			)
+		),
+		"Netherlands" => array(
+			"Amsterdam" => array(
+				"ams-a01.ipvanish.com",
+				"ams-a02.ipvanish.com",
+				"ams-a03.ipvanish.com",
+				"ams-a04.ipvanish.com",
+				"ams-a05.ipvanish.com",
+				"ams-a06.ipvanish.com",
+				"ams-a07.ipvanish.com",
+				"ams-a08.ipvanish.com",
+				"ams-a09.ipvanish.com",
+				"ams-a10.ipvanish.com",
+				"ams-a11.ipvanish.com",
+				"ams-a12.ipvanish.com",
+				"ams-a13.ipvanish.com",
+				"ams-a14.ipvanish.com",
+				"ams-c01.ipvanish.com"
+			)
+		),
+		"Norway" => array(
+			"Oslo" => array(
+				"osl-c01.ipvanish.com",
+				"osl-c02.ipvanish.com"
+			)
+		),
+		"Poland" => array(
+			"Warsaw" => array(
+				"waw-b01.ipvanish.com",
+				"waw-c01.ipvanish.com"
+			)
+		),
+		"Portugal" => array(
+			"Lisbon" => array(
+				""
+			)
+		),
+		"Romania" => array(
+			"Bucharest" => array(
+				"otp-c01.ipvanish.com",
+				"otp-c02.ipvanish.com"
+			)
+		),
+		"Russia" => array(
+			"Moscow" => array(
+				"dme-c01.ipvanish.com"
+			)
+		),
+		"Serbia" => array(
+			"Belgrade" => array(
+				"beg-c01.ipvanish.com"
+			)
+		),
+		"Slovak Republic" => array(
+			"Bratislava" => array(
+				"bts-c01.ipvanish.com"
+			)
+		),
+		"Slovenia" => array(
+			"Ljubljana" => array(
+				"lju-c01.ipvanish.com"
+			)
+		),
+		"Spain" => array(
+			"Madrid" => array(
+				"mad-a01.ipvanish.com",
+				"mad-a02.ipvanish.com",
+				"mad-c01.ipvanish.com"
+			)
+		),
+		"Sweden" => array(
+			"Stockholm" => array(
+				"sto-a01.ipvanish.com",
+				"sto-a02.ipvanish.com",
+				"sto-b01.ipvanish.com"
+			)
+		),
+		"Switzerland" => array(
+			"Zurich" => array(
+				"zur-c01.ipvanish.com",
+				"zur-c02.ipvanish.com",
+				"zur-b01.ipvanish.com"
+			)
+		),
+		"Ukraine" => array(
+			"Kharkiv" => array(
+				"hrk-c01.ipvanish.com"
+			)
+		),
+		"U.K." => array(
+			"London" => array(
+				"lon-a01.ipvanish.com",
+				"lon-a02.ipvanish.com",
+				"lon-a03.ipvanish.com",
+				"lon-a04.ipvanish.com",
+				"lon-a05.ipvanish.com",
+				"lon-a06.ipvanish.com",
+				"lon-a07.ipvanish.com",
+				"lon-a08.ipvanish.com",
+				"lon-a09.ipvanish.com",
+				"lon-a10.ipvanish.com",
+				"lon-a11.ipvanish.com",
+				"lon-c01.ipvanish.com",
+				"lon-c02.ipvanish.com"
+			),
+			"Manchester" => array(
+				"man-c01.ipvanish.com",
+				"man-c02.ipvanish.com",
+				"man-c03.ipvanish.com"
+			),
+			"Nottingham" => array(
+				"nqt-c01.ipvanish.com"
+			)
+		)
+		
+	),
+	"North America" => array(
+		"Canada" => array(
+			"Montreal" => array(
+				"yul-c01.ipvanish.com",
+				"yul-c01.ipvanish.com"
+			),
+			"Toronto" => array(
+				"tor-c01.ipvanish.com",
+				"tor-c02.ipvanish.com",
+				"tor-c03.ipvanish.com",
+				"tor-c04.ipvanish.com",
+				"yyz-c01.ipvanish.com",
+				"yyz-c02.ipvanish.com",
+				"yyz-c03.ipvanish.com"
+			),
+			"Vancouver" => array(
+				"yvr-c01.ipvanish.com",
+				"yvr-c02.ipvanish.com"
+			)
+		),
+		"Costa Rica" => array(
+			"San Jose" => array(
+				"sjo-c01.ipvanish.com"
+			)
+		),
+		"Mexico" => array(
+			//"Mexico City" => array(
+			//	""
+			//)
+			"Querétaro" => array(
+				"qro-c01.ipvanish.com"
+			)
+		),
+		"Panama" => array(
+			"Panama City" => array(
+				"pac-c01.ipvanish.com"
+			)
+		),
+		"U.S." => array(
+			"Ashburn, VA" => array(
+				""
+			),
+			"Atlanta, GA" => array(
+				""
+			),
+			"Chicago, IL" => array(
+				""
+			),
+			"Dallas, TX" => array(
+				""
+			),
+			"Las Vegas, NV" => array(
+				""
+			),
+			"Los Angeles, CA" => array(
+				""
+			),
+			"Miami, FL" => array(
+				""
+			),
+			"New York, NY" => array(
+				""
+			),
+			"Phoenix, AZ" => array(
+				""
+			),
+			"San Jose, CA" => array(
+				""
+			),
+			"Seattle, WA" => array(
+				""
+			)
+		)
+	),
+	"Central/South America" => array(
+		"Argentina" => array(
+			"Buenos Aires" => array(
+				"eze-c01.ipvanish.com"
+			)
+		),
+		"Brazil" => array(
+			"Sao Paulo" => array(
+				"gru-a01.ipvanish.com",
+				"gru-a02.ipvanish.com",
+				"gru-c01.ipvanish.com"
+			)
+		)
+	),
+	"Oceania" => array(
+		"Australia" => array(
+			"Melbourne" => array(
+				"mel-c01.ipvanish.com",
+				"mel-c02.ipvanish.com"
+			),
+			"Sydney" => array(
+				"syd-a01.ipvanish.com",
+				"syd-a02.ipvanish.com",
+				"syd-a01.ipvanish.com",
+				"syd-a02.ipvanish.com",
+				"syd-a03.ipvanish.com",
+				"syd-a04.ipvanish.com",
+				"syd-a05.ipvanish.com",
+				"syd-a06.ipvanish.com",
+				"syd-a07.ipvanish.com",
+				"syd-a08.ipvanish.com",
+				"syd-a09.ipvanish.com",
+				"syd-a10.ipvanish.com",
+				"syd-a11.ipvanish.com",
+				"syd-a12.ipvanish.com",
+				"syd-a13.ipvanish.com",
+				"syd-a14.ipvanish.com",
+				"syd-a15.ipvanish.com",
+				"syd-a16.ipvanish.com",
+				"syd-a17.ipvanish.com",
+				"syd-a18.ipvanish.com",
+				"syd-a19.ipvanish.com",
+				"syd-a20.ipvanish.com",
+				"syd-c01.ipvanish.com",
+				"syd-c02.ipvanish.com",
+				"syd-c03.ipvanish.com",
+				"syd-c04.ipvanish.com"
+			)
+		),
+		"New Zealand" => array(
+			"Auckland" => array(
+				"akl-c01.ipvanish.com",
+				"akl-c02.ipvanish.com",
+				"akl-c03.ipvanish.com",
+				"akl-c04.ipvanish.com",
+				"akl-c05.ipvanish.com",
+				"akl-c06.ipvanish.com",
+				"akl-c07.ipvanish.com",
+				"akl-c08.ipvanish.com"
+			)
+		)
+	),
+	"Asia" => array(
+		"China" => array(
+			"Hong Kong" => array(
+				"hkg-c01.ipvanish.com",
+				"hkg-c02.ipvanish.com",
+				"hkg-c03.ipvanish.com"
+			)
+		),
+		"India" => array(
+			"Mumbai" => array(
+				"bom-c01.ipvanish.com"
+			),
+			"New Delhi" => array(
+				"del-c01.ipvanish.com",
+				"del-c02.ipvanish.com"
+			)
+		),
+		"Indonesia" => array(
+			"Jakarta" => array(
+				"cgk-c01.ipvanish.com"
+			)
+		),
+		"Japan" => array(
+			"Tokyo" => array(
+				"nrt-a01.ipvanish.com",
+				"nrt-a02.ipvanish.com",
+				"nrt-a03.ipvanish.com",
+				"nrt-a04.ipvanish.com",
+				"nrt-a05.ipvanish.com",
+				"nrt-a06.ipvanish.com",
+				"nrt-c01.ipvanish.com"
+			)
+		),
+		"Kazakhstan" => array(
+			"Karaganda" => array(
+				"kgf-c01.ipvanish.com"
+			)
+		),
+		"Kyrgyzstan" => array(
+			"Bishkek" => array(
+				"fru-c01.ipvanish.com"
+			)
+		),
+		"Malaysia" => array(
+			"Kuala Lumpur" => array(
+				"kul-c01.ipvanish.com"
+			)
+		),
+		"Philippines" => array(
+			"Manila" => array(
+				"mnl-a01.ipvanish.com",
+				"mnl-a02.ipvanish.com",
+				"mnl-a03.ipvanish.com",
+				"mnl-a04.ipvanish.com"
+			)
+		),
+		"Saudi Arabia" => array(
+			"Riyadh" => array(
+				"ruh-c01.ipvanish.com"
+			)
+		),
+		"Singapore" => array(
+			"Serangoon" => array(
+				"sin-a01.ipvanish.com",
+				"sin-a02.ipvanish.com",
+				"sin-c01.ipvanish.com"
+			)
+		),
+		"South Korea" => array(
+			"Seoul" => array(
+				"sel-a01.ipvanish.com",
+				"sel-a02.ipvanish.com",
+				"sel-a03.ipvanish.com"
+			)
+		),
+		"Thailand" => array(
+			"Bangkok" => array(
+				"bkk-c01.ipvanish.com"
+			)
+		),
+		"Turkey" => array(
+			"Bursa" => array(
+				"yei-c01.ipvanish.com"
+			),
+			"Istanbul" => array(
+				"ist-c01.ipvanish.com",
+				"ist-c02.ipvanish.com",
+				"ist-c03.ipvanish.com"
+			),
+			"Izmir" => array(
+				"izm-c01.ipvanish.com"
+			)
+		)
+	),
+	"Africa" => array(
+		"Egypt" => array(
+			"Cairo" => array(
+				"cai-c01.ipvanish.com"
+			)
+		),
+		"South Africa" => array(
+			"Johannesburg" => array(
+				"jnb-c01.ipvanish.com",
+				"jnb-c02.ipvanish.com",
+				"jnb-c03.ipvanish.com",
+				"jnb-c04.ipvanish.com",
+				"jnb-c05.ipvanish.com"
+			)
+		)
+	)
 );
 
 $list = "# List distributed by http://code.luenwarneke.com/ipvanish\n";
@@ -155,13 +539,35 @@ IPVanish VPN network/local/netmask:255.255.252.0-255.255.252.0
 IPVanish VPN MASK:0.0.0.0-0.0.0.0
 IPVanish VPN MASK:128.0.0.0-128.0.0.0\n";
 
-//$iplist = array();
-foreach ($domainlist as $hostname) {
-	$hosts = gethostbynamel($hostname);
-	if (is_array($hosts)) {
-		foreach ($hosts as $ip) {
-			//$iplist[] = $ip;
-			$list .= "IPVanish VPN ".$hostname.":".$ip."-".$ip."\n";
+
+function ip_range($start, $end) {
+	$start = ip2long($start);
+	$end = ip2long($end);
+	$range = range($start, $end);
+	return array_map('long2ip', $range);
+}
+
+$domain = "www.ipvanish.com";
+$hosts = gethostbynamel($domain);
+if (is_array($hosts)) {
+	foreach ($hosts as $ip) {
+		$list .= "IPVanish ".$hostname.":".$ip."-".$ip."\n";
+	}
+}
+
+foreach ($serverlist as $continent) {
+	foreach ($continent as $country) {
+		foreach ($country as $city) {
+			foreach ($city as $hostname) {
+				$hosts = gethostbynamel($hostname);
+				if (is_array($hosts)) {
+					foreach ($hosts as $ip) {
+						$servertype = preg_replace("/(?:[a-z])*-([a-z])[0-9]{2}.ipvanish.com/", "$1", $hostname);
+						$own = ($servertype == 'a' || $servertype == 'b' ? ' (Owns Data Centre)' : '');
+						$list .= "IPVanish ".$hostname.$own.":".$ip."-".$ip."\n";
+					}
+				}
+			}
 		}
 	}
 }
